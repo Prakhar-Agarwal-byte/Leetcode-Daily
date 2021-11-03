9501f9603c99a7f1f79fd8ce5f3544a5fc3b0885
Prakhar Agarwal <prakhar@everything.net.in>
2021-11-03 15:50:32 +0530

    Create Sort Colors.java

---
+++ Sort Colors.java
@@ -0,0 +1,27 @@
+class Solution {
+  public void sortColors(int[] nums) {
+    int low = 0, mid = 0, high = nums.length - 1;
+    while (mid <= high) {
+      switch (nums[mid]) {
+      case 0:
+        swap(low, mid, nums);
+        low++;
+        mid++;
+        break;
+      case 1:
+        mid++;
+        break;
+      case 2:
+        swap(mid, high, nums);
+        high--;
+        break;
+      }
+    }
+  }
+
+  private void swap(int i, int j, int[] nums) {
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+  }
+}
